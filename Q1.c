50779d461129d64b911768fc52fe0fc68fbaae27
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2025-01-14 13:42:55 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,7 @@
+Q. Consider an implementation of unsorted single linked list. 
+Suppose it has its representation with a head and a tail pointer 
+(i.e. pointers to the first and last nodes of the linked list). 
+Given the representation, which of the following operation can not be implemented in O(1) time?
+
+
+d) Deletion of the last node of the linked list
